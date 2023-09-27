0d9f3ad9e729f42a2a9e70424988357f5142fa8d
Guilherme <guilhermelopes035@gmail.com>
2023-09-26 22:10:10 -0300

    feat: adiciona classe Photo

--- src/main/java/usecase/Photo.java
+++ src/main/java/usecase/Photo.java
@@ -1,2 +1,17 @@
-package usecase;public class Photo {
+package usecase;
+
+public class Photo {
+
+    private final int width;
+    private final int height;
+
+    public Photo(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int area() {
+        return width * height;
+    }
+
 }
