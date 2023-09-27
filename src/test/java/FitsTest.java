72e5083a06d464b375bc3b2199296c6c50451505
Guilherme <guilhermelopes035@gmail.com>
2023-09-26 22:15:59 -0300

    refactor: refatora casos de teste

--- src/test/java/FitsTest.java
+++ src/test/java/FitsTest.java
@@ -1,35 +1,49 @@
-import usecase.Fits;
+import usecase.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class FitsTest {
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+class FitsTest {
     @Test
-    @DisplayName("Teste de Encaixe - Pode Encaixar")
-    public void testCanFit() {
+    @DisplayName("Deve retornar verdadeiro se as fotos se encaixarem horizontalmente")
+    void shouldReturnTrueIfPhotosFitHorizontally() {
         Photo photo1 = new Photo(3, 4);
         Photo photo2 = new Photo(3, 4);
-        Fits fits1 = new Fits(photo1, photo2, 7, 5);
-        assertThat(fits1.handle()).isTrue();
-
-        Photo photo3 = new Photo(6, 6);
-        Photo photo4 = new Photo(6, 6);
-        Fits fits2 = new Fits(photo3, photo4, 10, 10);
-        assertThat(fits2.handle()).isTrue();
+        Fits fits = new Fits(photo1, photo2, 7, 5);
+        assertThat(fits.handle()).isTrue();
     }
 
     @Test
-    @DisplayName("Teste de Encaixe - Não Pode Encaixar")
-    public void testCannotFit() {
+    @DisplayName("Deve retornar verdadeiro se as fotos se encaixarem verticalmente")
+    void shouldReturnTrueIfPhotosFitVertically() {
+        Photo photo1 = new Photo(3, 4);
+        Photo photo2 = new Photo(3, 4);
+        Fits fits = new Fits(photo1, photo2, 7, 5);
+        assertThat(fits.handle()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar falso se as fotos não se encaixarem")
+    void shouldReturnFalseIfPhotosDoNotFit() {
         Photo photo1 = new Photo(3, 3);
         Photo photo2 = new Photo(3, 3);
-        Fits fits3 = new Fits(photo1, photo2, 4, 3);
-        assertThat(fits3.handle()).isFalse();
+        Fits fits = new Fits(photo1, photo2, 4, 3);
+        assertThat(fits.handle()).isFalse();
+    }
 
-        Photo photo3 = new Photo(4, 4);
-        Photo photo4 = new Photo(4, 4);
-        Fits fits4 = new Fits(photo3, photo4, 6, 4);
-        assertThat(fits4.handle()).isFalse();
+    @Test
+    @DisplayName("Deve falhar no teste devido ao uso desnecessário de stubbing")
+    void shouldFailTestDueToUnnecessaryStubbing() {
+        Photo photo1 = new Photo(3, 3);
+        Photo photo2 = new Photo(3, 3);
+        Fits fits = new Fits(photo1, photo2, 4, 3);
+        assertThatIllegalArgumentException().isThrownBy(() -> fits.handle());
     }
 }
