3ed2f69da13fee559ff122d60fd5187aac76e045
Guilherme <guilhermelopes035@gmail.com>
2023-09-26 22:36:22 -0300

    refactor: refatora casos de teste para melhor teste da logica

--- src/test/java/FitsTest.java
+++ src/test/java/FitsTest.java
@@ -3,47 +3,48 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FitsTest {
+
     @Test
-    @DisplayName("Deve retornar verdadeiro se as fotos se encaixarem horizontalmente")
+    @DisplayName("Deve retornar verdadeiro se as fotos couberem horizontalmente na página do álbum")
     void shouldReturnTrueIfPhotosFitHorizontally() {
         Photo photo1 = new Photo(3, 4);
         Photo photo2 = new Photo(3, 4);
-        Fits fits = new Fits(photo1, photo2, 7, 5);
+        Fits fits = new Fits(photo1, photo2, 5, 7);
         assertThat(fits.handle()).isTrue();
     }
 
     @Test
-    @DisplayName("Deve retornar verdadeiro se as fotos se encaixarem verticalmente")
+    @DisplayName("Deve retornar verdadeiro se as fotos couberem verticalmente na página do álbum")
     void shouldReturnTrueIfPhotosFitVertically() {
-        Photo photo1 = new Photo(3, 4);
-        Photo photo2 = new Photo(3, 4);
+        Photo photo1 = new Photo(4, 3);
+        Photo photo2 = new Photo(4, 3);
         Fits fits = new Fits(photo1, photo2, 7, 5);
         assertThat(fits.handle()).isTrue();
     }
 
     @Test
-    @DisplayName("Deve retornar falso se as fotos não se encaixarem")
-    void shouldReturnFalseIfPhotosDoNotFit() {
-        Photo photo1 = new Photo(3, 3);
-        Photo photo2 = new Photo(3, 3);
-        Fits fits = new Fits(photo1, photo2, 4, 3);
-        assertThat(fits.handle()).isFalse();
+    @DisplayName("Deve retornar verdadeiro se as fotos couberem horizontalmente e verticalmente na página do álbum")
+    void shouldReturnTrueIfPhotosFitBothHorizontallyAndVertically() {
+        Photo photo1 = new Photo(2, 3);
+        Photo photo2 = new Photo(3, 2);
+        Fits fits = new Fits(photo1, photo2, 3, 3);
+        assertThat(fits.handle()).isTrue();
     }
 
     @Test
-    @DisplayName("Deve falhar no teste devido ao uso desnecessário de stubbing")
-    void shouldFailTestDueToUnnecessaryStubbing() {
-        Photo photo1 = new Photo(3, 3);
-        Photo photo2 = new Photo(3, 3);
-        Fits fits = new Fits(photo1, photo2, 4, 3);
-        assertThatIllegalArgumentException().isThrownBy(() -> fits.handle());
+    @DisplayName("Deve retornar falso se as fotos não couberem nem horizontalmente nem verticalmente na página do álbum")
+    void shouldReturnFalseIfPhotosDoNotFitHorizontallyNorVertically() {
+        Photo photo1 = new Photo(5, 7);
+        Photo photo2 = new Photo(7, 5);
+        Fits fits = new Fits(photo1, photo2, 5, 7);
+        assertThat(fits.handle()).isFalse();
     }
 }
