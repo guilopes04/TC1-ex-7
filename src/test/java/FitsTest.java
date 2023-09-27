2cf5960f4135e33350bc0db74b952c8c61d3e7b3
Guilherme <guilhermelopes035@gmail.com>
2023-09-26 22:42:00 -0300

    chore: retira import nao utilizados

--- src/test/java/FitsTest.java
+++ src/test/java/FitsTest.java
@@ -2,12 +2,8 @@ import usecase.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FitsTest {
